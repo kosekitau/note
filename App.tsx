36c16f43c14e5f4818be94791b54157d18b90e23
koseki <kosekitau@gmail.com>
2021-05-04 16:27:06 +0900

    add color

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faHeart,
-} from "@fortawesome/free-solid-svg-icons";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
@@ -11,21 +8,27 @@ library.add(fas, far)//あらかじめ使用するアイコンを追加してお
 
 const App = () => {
     const [iine, setIine] = useState(false);
+    const [iines, setIines] = useState(0);
     
     const hanten = () =>{
         const iine2 =  !iine;
+        const iines2 = (iines+1)%2;
         setIine(iine2);
+        setIines(iines2);
     };
     
     return (
-      <div className='iine_button' onClick={() => hanten()}>
-          {iine ? (
-              <FontAwesomeIcon icon={['fas', 'heart']} />
-            ) : (
-              <FontAwesomeIcon icon={['far', 'heart']} />
-          )
-          }
-      </div>
+        <div className="app-background">
+            <div className='iine_button' onClick={() => hanten()}>
+                {iine ? (
+                    <FontAwesomeIcon icon={['fas', 'heart']} style={{color:'pink'}} fixedWidth/>
+                    ) : (
+                    <FontAwesomeIcon icon={['far', 'heart']} style={{color:'pink'}} fixedWidth/>
+                    )
+                }
+                <span>{iines}</span>
+            </div>
+        </div>
     );
 }
   
