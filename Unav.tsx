a3ea631cc00ec1c7c73faee171243ad130e34c35
koseki <kosekitau@gmail.com>
2021-05-15 22:42:53 +0900

    add handleShow

--- Unav.tsx
+++ Unav.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Unav.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -8,26 +9,52 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fas, far, fab)//あらかじめ使用するアイコンを追加しておく
 
 export const Unav = () => {
-    const [iine, setIine] = useState(false);
-    const [iines, setIines] = useState(0);
+    const [good, setIine] = useState(false);
+    const [count_good, setIines] = useState(0);
+    const [show, setShow] = useState(false);
+    const [hh, setHh] = useState(0);
     
+    useEffect(() => {
+        //一定数スクロールしたら-footer-animationクラスを付与し、
+        //そのクラスにscssが反応する
+        const handleShow = () => {
+          //window.scrollYでどのくらいスクロールしたかを取得
+          let nowh = window.scrollY;
+          setHh(nowh);
+          //下にスクロールしたら
+          if (nowh > hh) {
+            setShow(true);
+            console.log(hh, nowh);
+          } else {
+            setShow(false);
+            console.log(hh, nowh);
+          }
+        };
+        
+        //少しでもスクロールしたら
+        window.addEventListener("scroll", handleShow);
+        return () => {
+          window.removeEventListener("scroll", handleShow);
+        };
+    });//この[]に引数を入れる
+
     const hanten = () =>{
-        const iine2 =  !iine;
-        const iines2 = (iines+1)%2;
-        setIine(iine2);
-        setIines(iines2);
+        const good2 = !good;
+        const count_good2 = (count_good+1)%2;
+        setIine(good2);
+        setIines(count_good2);
     };
     
     return (
-        <div className="app-background">
+        <div className={`Unav ${show && "Unav-footer-animation"}`}>
             <div className='iine_button' onClick={() => hanten()}>
-                {iine ? (
+                {good ? (
                     <FontAwesomeIcon icon={['fas', 'heart']} style={{color:'pink'}} size='2x' fixedWidth/>
                     ) : (
                     <FontAwesomeIcon icon={['far', 'heart']} style={{color:'pink'}} size='2x' fixedWidth/>
                     )
                 }
-                <span>{iines}</span>
+                <span>{count_good}</span>
                 <FontAwesomeIcon icon={['fas', 'plus']} style={{color:'gray'}} size='2x'/>
                 <FontAwesomeIcon icon={['far', 'clone']} style={{color:'gray'}} size='2x'/>
                 <FontAwesomeIcon icon={['fab', 'twitter']} style={{color:'gray'}} size='2x'/>
