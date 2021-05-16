47c2b3b64e4f98c493ca5d32332386e223177394
koseki <kosekitau@gmail.com>
2021-05-16 09:09:38 +0900

    add Nav

---
+++ Nav.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)//あらかじめ使用するアイコンを追加しておく
+
+export const Nav = () => {
+    const [show, setShow] = useState(false);
+    const [hh, setHh] = useState(0);
+    
+    useEffect(() => {
+        //一定数スクロールしたら-footer-animationクラスを付与し、
+        //そのクラスにscssが反応する
+        const handleShow = () => {
+          //window.scrollYでどのくらいスクロールしたかを取得
+          let nowh = window.scrollY;
+          setHh(nowh);
+          //下にスクロールしたら
+          if (nowh > hh) {
+            setShow(true);
+            console.log(hh, nowh);
+          } else {
+            setShow(false);
+            console.log(hh, nowh);
+          }
+        };
+        
+        //少しでもスクロールしたら
+        window.addEventListener("scroll", handleShow);
+        return () => {
+          window.removeEventListener("scroll", handleShow);
+        };
+    });//この[]に引数を入れる
+    return (
+        <div className={`Nav ${show && "Nav-footer-animation"}`}>
+            <div className='nav'>
+                <FontAwesomeIcon icon={['fas', 'search']} style={{color:'grey'}} size='2x'/>
+                <p>ログイン</p>
+                <input type="button" id="button1" value="新規登録"></input>
+            </div>
+        </div>
+    );
+}
--- Unav.tsx
+++ Unav.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./Unav.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fas, far, fab)//あらかじめ使用するアイコンを追加しておく
